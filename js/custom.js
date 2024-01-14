9ca4da91e1f2ec2556487d11dae956e59b78dee2
hishobkar <abhishobkar@gmail.com>
2024-01-14 19:25:53 +0530

    update qr code to all pages

--- js/custom.js
+++ js/custom.js
@@ -2,17 +2,40 @@
 
 // PRELOADER
 
-$(window).load(function(){
-    $('.preloader').delay(1000).fadeOut("slow"); // set duration in brackets    
+$(window).load(function () {
+	$('.preloader').delay(1000).fadeOut("slow"); // set duration in brackets    
 });
 
 // HOME BACKGROUND SLIDESHOW
-$(function(){
-    jQuery(document).ready(function() {
+$(function () {
+	jQuery(document).ready(function () {
+
+		// // start - add libries dynamically
+		// const libraries = [
+		// 	// 'https://hishobkar.github.io/js/qrcode.min.js',
+		// 	// 'https://cdn.jsdelivr.net/gh/davidshimjs/qrcodejs@gh-pages/qrcode.min.js',
+		// 	// './js/library2.js',
+		// 	// ... paths to the other libraries
+		// ];
+
+		// libraries.forEach(libraryPath => {
+		// 	const script = document.createElement('script');
+		// 	script.src = libraryPath;
+		// 	document.head.appendChild(script);
+		// });
+		// // end - add libries dynamically
+
 		$('body').backstretch([
-	 		 "images/tm-bg-slide-1.jpg", 
-	 		 "images/tm-bg-slide-2.jpg",
-			 "images/tm-bg-slide-3.jpg"
-	 			], 	{duration: 3200, fade: 1300});
-		});
+			"images/tm-bg-slide-1.jpg",
+			"images/tm-bg-slide-2.jpg",
+			"images/tm-bg-slide-3.jpg"
+		], { duration: 3200, fade: 1300 });
+
+		var currentBlogPostLink = window.location.href;
+		var options = { width: 80, height: 80 };
+
+		var qrcode = new QRCode(document.getElementById("qrcode"), options);
+		
+		qrcode.makeCode(currentBlogPostLink);
+	});
 })
