658316fa9b1f309737b5cb9906b3db619ef16baa
Abhijeet <abhishobkar@gmail.com>
2024-01-14 22:34:47 +0530

    Update custom.js
    reverting code to commit - cd91dcb28c6c49686b0baeaf2b03e216417a44d1

--- js/custom.js
+++ js/custom.js
@@ -10,39 +10,6 @@ $(window).load(function () {
 
         // Check screen width on page load
         $(document).ready(function () {
-            checkScreenWidth();
-
-            // Check screen width on window resize
-            $(window).resize(function () {
-                checkScreenWidth();
-            });
-        });
-
-        // Function to check screen width and apply class accordingly
-        function checkScreenWidth() {
-            var screenWidth = $(window).width();
-
-            // Set a threshold width for mobile viewers, adjust as needed
-            var thresholdWidth = 768;
-
-            if (screenWidth < thresholdWidth) {
-                // Apply class or make adjustments for mobile version
-                // For example, you could add a class to the body element:
-                // $('body').addClass('mobile-view');
-            } else {
-                // Remove the class or reset adjustments for desktop version
-                // For example, remove the previously added class:
-                // $('body').removeClass('mobile-view');
-            }
-        }
-
-
-// HOME BACKGROUND SLIDESHOW
-$(function () {
-	jQuery(document).ready(function () {
-
-
-
 // start - restrict mobile users to view mobile version of website
 
 // Disable viewport meta tag for Bootstrap's responsiveness
@@ -52,8 +19,13 @@ $(function () {
 // const mqs = document.querySelectorAll('link[media]');
 // mqs.forEach(mq => mq.remove());
 // end - restrict mobile users to view mobile version of website
+        });
 
 
+
+// HOME BACKGROUND SLIDESHOW
+$(function () {
+	jQuery(document).ready(function () {
 
 
 		// // start - add libries dynamically
