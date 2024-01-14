b0b6eed88898a65486d9fea0046cc77b7e997378
Abhijeet <abhishobkar@gmail.com>
2024-01-14 22:25:03 +0530

    Update custom.js
    chatgpt code for best mobile viewers

--- js/custom.js
+++ js/custom.js
@@ -6,6 +6,37 @@ $(window).load(function () {
 	$('.preloader').delay(1000).fadeOut("slow"); // set duration in brackets    
 });
 
+
+
+        // Check screen width on page load
+        $(document).ready(function () {
+            checkScreenWidth();
+
+            // Check screen width on window resize
+            $(window).resize(function () {
+                checkScreenWidth();
+            });
+        });
+
+        // Function to check screen width and apply class accordingly
+        function checkScreenWidth() {
+            var screenWidth = $(window).width();
+
+            // Set a threshold width for mobile viewers, adjust as needed
+            var thresholdWidth = 768;
+
+            if (screenWidth < thresholdWidth) {
+                // Apply class or make adjustments for mobile version
+                // For example, you could add a class to the body element:
+                // $('body').addClass('mobile-view');
+            } else {
+                // Remove the class or reset adjustments for desktop version
+                // For example, remove the previously added class:
+                // $('body').removeClass('mobile-view');
+            }
+        }
+
+
 // HOME BACKGROUND SLIDESHOW
 $(function () {
 	jQuery(document).ready(function () {
@@ -20,12 +51,6 @@ $(function () {
 // Optionally, remove Bootstrap's responsive media queries
 // const mqs = document.querySelectorAll('link[media]');
 // mqs.forEach(mq => mq.remove());
-
-
-
-$('meta[name="viewport"]').attr('content', 'width=device-width, initial-scale=1.0');
-$('link[media]').remove();
-
 // end - restrict mobile users to view mobile version of website
 
 
